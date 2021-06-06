52c98c740fa248b6b49282958c7280753922601d
MarianoLuque <83260550+MarianoLuque@users.noreply.github.com>
2021-06-05 23:18:00 -0300

    Login y Diagrama de secuencia

--- DSI-PPAI/DSI-PPAI/Forms/login.Designer.cs
+++ DSI-PPAI/DSI-PPAI/Forms/login.Designer.cs
@@ -30,25 +30,18 @@
         {
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.bunifuElipse1 = new Bunifu.Framework.UI.BunifuElipse(this.components);
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.txt_password = new System.Windows.Forms.TextBox();
+            this.txt_usuario = new System.Windows.Forms.TextBox();
+            this.btn_ver_contrasena = new System.Windows.Forms.Button();
+            this.btn_ocultar_contrasena = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btn_aceptar = new System.Windows.Forms.Button();
+            this.btn_cancelar = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
-            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
-            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBox1.Location = new System.Drawing.Point(475, 313);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(132, 62);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
             // 
             // pictureBox3
             // 
@@ -66,16 +59,104 @@
             this.bunifuElipse1.ElipseRadius = 40;
             this.bunifuElipse1.TargetControl = this;
             // 
-            // pictureBox2
+            // txt_password
             // 
-            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
-            this.pictureBox2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.BackgroundImage")));
-            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBox2.Location = new System.Drawing.Point(638, 313);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(132, 62);
-            this.pictureBox2.TabIndex = 3;
-            this.pictureBox2.TabStop = false;
+            this.txt_password.Location = new System.Drawing.Point(499, 279);
+            this.txt_password.Name = "txt_password";
+            this.txt_password.PasswordChar = '*';
+            this.txt_password.Size = new System.Drawing.Size(211, 20);
+            this.txt_password.TabIndex = 20;
+            // 
+            // txt_usuario
+            // 
+            this.txt_usuario.Location = new System.Drawing.Point(500, 222);
+            this.txt_usuario.Name = "txt_usuario";
+            this.txt_usuario.Size = new System.Drawing.Size(210, 20);
+            this.txt_usuario.TabIndex = 19;
+            // 
+            // btn_ver_contrasena
+            // 
+            this.btn_ver_contrasena.BackColor = System.Drawing.Color.Transparent;
+            this.btn_ver_contrasena.BackgroundImage = global::DSI_PPAI.Properties.Resources.vistoo;
+            this.btn_ver_contrasena.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_ver_contrasena.Location = new System.Drawing.Point(716, 278);
+            this.btn_ver_contrasena.Name = "btn_ver_contrasena";
+            this.btn_ver_contrasena.Size = new System.Drawing.Size(34, 26);
+            this.btn_ver_contrasena.TabIndex = 17;
+            this.btn_ver_contrasena.UseVisualStyleBackColor = false;
+            this.btn_ver_contrasena.Click += new System.EventHandler(this.btn_ver_contrasena_Click);
+            // 
+            // btn_ocultar_contrasena
+            // 
+            this.btn_ocultar_contrasena.BackColor = System.Drawing.Color.Transparent;
+            this.btn_ocultar_contrasena.BackgroundImage = global::DSI_PPAI.Properties.Resources.novistoo;
+            this.btn_ocultar_contrasena.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_ocultar_contrasena.Location = new System.Drawing.Point(716, 278);
+            this.btn_ocultar_contrasena.Name = "btn_ocultar_contrasena";
+            this.btn_ocultar_contrasena.Size = new System.Drawing.Size(34, 26);
+            this.btn_ocultar_contrasena.TabIndex = 18;
+            this.btn_ocultar_contrasena.UseVisualStyleBackColor = false;
+            this.btn_ocultar_contrasena.Click += new System.EventHandler(this.btn_ocultar_contrasena_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(499, 256);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(78, 20);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Password";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(500, 199);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 20);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Usuario";
+            // 
+            // btn_aceptar
+            // 
+            this.btn_aceptar.BackColor = System.Drawing.Color.Transparent;
+            this.btn_aceptar.BackgroundImage = global::DSI_PPAI.Properties.Resources.Button;
+            this.btn_aceptar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_aceptar.FlatAppearance.BorderSize = 0;
+            this.btn_aceptar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_aceptar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_aceptar.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn_aceptar.Location = new System.Drawing.Point(499, 331);
+            this.btn_aceptar.Margin = new System.Windows.Forms.Padding(0);
+            this.btn_aceptar.Name = "btn_aceptar";
+            this.btn_aceptar.Size = new System.Drawing.Size(113, 56);
+            this.btn_aceptar.TabIndex = 22;
+            this.btn_aceptar.Text = "Aceptar";
+            this.btn_aceptar.UseVisualStyleBackColor = false;
+            this.btn_aceptar.Click += new System.EventHandler(this.btn_aceptar_Click);
+            // 
+            // btn_cancelar
+            // 
+            this.btn_cancelar.BackColor = System.Drawing.Color.Transparent;
+            this.btn_cancelar.BackgroundImage = global::DSI_PPAI.Properties.Resources.Button;
+            this.btn_cancelar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_cancelar.FlatAppearance.BorderSize = 0;
+            this.btn_cancelar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_cancelar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_cancelar.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn_cancelar.Location = new System.Drawing.Point(637, 331);
+            this.btn_cancelar.Margin = new System.Windows.Forms.Padding(0);
+            this.btn_cancelar.Name = "btn_cancelar";
+            this.btn_cancelar.Size = new System.Drawing.Size(113, 56);
+            this.btn_cancelar.TabIndex = 22;
+            this.btn_cancelar.Text = "Cancelar";
+            this.btn_cancelar.UseVisualStyleBackColor = false;
+            this.btn_cancelar.Click += new System.EventHandler(this.btn_cancelar_Click);
             // 
             // Login
             // 
@@ -84,9 +165,15 @@
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.btn_cancelar);
+            this.Controls.Add(this.btn_aceptar);
+            this.Controls.Add(this.txt_password);
+            this.Controls.Add(this.txt_usuario);
+            this.Controls.Add(this.btn_ver_contrasena);
+            this.Controls.Add(this.btn_ocultar_contrasena);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.pictureBox3);
-            this.Controls.Add(this.pictureBox1);
             this.DoubleBuffered = true;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Login";
@@ -94,19 +181,24 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Login";
             this.Load += new System.EventHandler(this.Login_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private Bunifu.Framework.UI.BunifuElipse bunifuElipse1;
-        private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.PictureBox pictureBox3;
-        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TextBox txt_password;
+        private System.Windows.Forms.TextBox txt_usuario;
+        private System.Windows.Forms.Button btn_ver_contrasena;
+        private System.Windows.Forms.Button btn_ocultar_contrasena;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btn_cancelar;
+        private System.Windows.Forms.Button btn_aceptar;
     }
 }
 
--- DSI-PPAI/DSI-PPAI/Forms/login.cs
+++ DSI-PPAI/DSI-PPAI/Forms/login.cs
@@ -12,6 +12,19 @@ namespace DSI_PPAI
 {
     public partial class Login : Form
     {
+        public String Pp_usuario
+        {
+            get { return txt_usuario.Text; }
+            set { txt_usuario.Text = value; }
+        }
+
+
+        public String Pp_password
+        {
+            get { return txt_password.Text; }
+            set { txt_password.Text = value; }
+        }
+
         public Login()
         {
             InitializeComponent();
@@ -19,7 +32,42 @@ namespace DSI_PPAI
 
         private void Login_Load(object sender, EventArgs e)
         {
+            btn_ver_contrasena.Hide();
+        }
 
+        private void btn_aceptar_Click(object sender, EventArgs e)
+        {
+            if (txt_usuario.Text == "")
+            {
+                MessageBox.Show("La casilla de USUARIO se encuentra vacia");
+                txt_usuario.Focus();
+                return;
+            }
+            if (txt_password.Text == "")
+            {
+                MessageBox.Show("La casilla de PASSWORD se encuentra vacia");
+                txt_password.Focus();
+                return;
+            }
+        }
+
+        private void btn_cancelar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btn_ver_contrasena_Click(object sender, EventArgs e)
+        {
+            txt_password.PasswordChar = '*';
+            btn_ver_contrasena.Hide();
+            btn_ocultar_contrasena.Show();
+        }
+
+        private void btn_ocultar_contrasena_Click(object sender, EventArgs e)
+        {
+            txt_password.PasswordChar = '\0';
+            btn_ocultar_contrasena.Hide();
+            btn_ver_contrasena.Show();
         }
     }
 }
